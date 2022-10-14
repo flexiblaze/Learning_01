6d46fa5d90137c9f5c0549b82484de0bed25d11f
flexiblaze <ilikcay24@gmail.com>
2022-10-14 09:06:57 +0200

    function.php added

---
+++ project/php_exercise/function.php
@@ -0,0 +1,19 @@
+<?php
+
+function optellen($nummer1,$nummer2){
+    return ($nummer1+$nummer2);
+}
+echo optellen(3,5);
+
+
+function ver($nummer1,$nummer2){
+    return ($nummer1*$nummer2);
+}
+echo ver(6,7);
+
+
+function delen($nummer1,$nummer2){
+    return ($nummer1/$nummer2);
+}
+echo delen(6,3);
+
